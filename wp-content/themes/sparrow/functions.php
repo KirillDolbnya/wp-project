143cd75ff7e82dbf20ff1f398a52af9e8c8da877
DESKTOP-HKH01U1\админ <kirilldolbnya6@gmail.com>
2023-08-12 22:44:19 +0300

    регистрация sidebar + правка стилей

--- wp-content/themes/sparrow/functions.php
+++ wp-content/themes/sparrow/functions.php
@@ -4,6 +4,23 @@ add_action( 'wp_enqueue_scripts', 'style_theme' );
 add_action('wp_footer','script_theme');
 add_action( 'init', 'register_jquery' );
 add_action( 'after_setup_theme', 'theme_register_nav_menu' );
+add_action( 'widgets_init', 'register_my_sidebar' );
+
+function register_my_sidebar(){
+
+    register_sidebar( array(
+        'name'          => 'right sidebar',
+        'id'            => "right-sidebar",
+        'description'   => 'Описание сайдбара',
+        'class'         => '',
+        'before_widget' => '<div class="widget %2$s">',
+        'after_widget'  => "</div>\n",
+//        'before_title'  => '<h5 class="widget-title %2$s">',
+//        'after_title'   => "</h5>\n",
+        'before_sidebar' => '', // WP 5.6
+        'after_sidebar'  => '', // WP 5.6
+    ) );
+}
 
 function theme_register_nav_menu() {
     register_nav_menu( 'header', 'верхнее меню' );
---
+++ wp-content/themes/sparrow/sidebar.php
@@ -0,0 +1,21 @@
+<aside id="sidebar">
+
+    <?php
+        dynamic_sidebar('right-sidebar');
+    ?>
+
+    <div class="widget widget_photostream">
+        <h5>Photostream</h5>
+        <ul class="photostream cf">
+            <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+            <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+            <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+            <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+            <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+            <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+            <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+            <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+        </ul>
+    </div>
+
+</aside>
