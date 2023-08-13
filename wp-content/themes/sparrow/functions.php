110bbb6cb45fff991ee23d4b98f0773aa05d4a61
DESKTOP-HKH01U1\админ <kirilldolbnya6@gmail.com>
2023-08-13 15:21:54 +0300

    вывод постов, регистрация размера фотографий для потса, add title-tag, add pagination, add filter for excerpt

--- wp-content/themes/sparrow/functions.php
+++ wp-content/themes/sparrow/functions.php
@@ -25,6 +25,39 @@ function register_my_sidebar(){
 function theme_register_nav_menu() {
     register_nav_menu( 'header', 'верхнее меню' );
     register_nav_menu( 'footer', 'нижнее меню' );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails', array( 'post' ) );
+    add_image_size('post_thumb', 1300,500,true);
+    add_filter( 'excerpt_length', function(){
+        return 35;
+    } );
+    add_filter( 'excerpt_more', 'new_excerpt_more' );
+    function new_excerpt_more( $more ){
+        global $post;
+        return '<a href="'. get_permalink($post) . '"> Читать дальше...</a>';
+    }
+    // удаляет H2 из шаблона пагинации
+    add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
+    function my_navigation_template( $template, $class ){
+        /*
+        Вид базового шаблона:
+        <nav class="navigation %1$s" role="navigation">
+            <h2 class="screen-reader-text">%2$s</h2>
+            <div class="nav-links">%3$s</div>
+        </nav>
+        */
+
+        return '
+	<nav class="navigation %1$s" role="navigation">
+		<div class="nav-links">%3$s</div>
+	</nav>
+	';
+    }
+
+// выводим пагинацию
+    the_posts_pagination( array(
+        'end_size' => 2,
+    ) );
 }
 
 function style_theme()
